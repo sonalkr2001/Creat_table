0e8a33922db196ba7a85d55df0be9f984161374e
SONAL KUMAR <sonalkr54@gmail.com>
2022-04-04 08:06:11 +0530

    Add files via upload

---
+++ table.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class table
+{
+  public static void main(String ar[])
+  {
+    Scanner sc=new Scanner(System.in);
+	int i=1,t,n;
+	System.out.println("enter any no");
+	n=sc.nextInt();
+	while(i<=10)
+	{
+	  t=i*n;
+	  System.out.println(t);
+	  i++;
+	 }
+	}
+}
+
+	 
